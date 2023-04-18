45a600a47692a8a2eda2ae37a766e7834e4784ab
y6mc37021 <tkarthik21@gmail.com>
2023-04-18 13:49:03 +0100

    Spring Cloud API-Gateway Routes

--- api-gateway/src/main/java/com/company/microservice/apigateway/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/company/microservice/apigateway/ApiGatewayConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/* Spring Cloud API-Gateway Routes Example*/
 @Configuration
 public class ApiGatewayConfiguration {
 	
