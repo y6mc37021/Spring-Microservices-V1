5c560b78c5f213f8e6405fb2bb77b8f71187609a
y6mc37021 <tkarthik21@gmail.com>
2023-04-19 23:12:12 +0100

    Introduction to Zipkins

--- currency-exchange-service/src/main/java/com/company/microservice/currencyexchangeservice/controller/CurrencyExchangeServiceController.java
+++ currency-exchange-service/src/main/java/com/company/microservice/currencyexchangeservice/controller/CurrencyExchangeServiceController.java
@@ -1,5 +1,7 @@
 package com.company.microservice.currencyexchangeservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +20,12 @@ public class CurrencyExchangeServiceController {
 	@Autowired
 	private CurrencyExchangeServiceRepository repository;
 	
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeServiceController.class);
+	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retiveExchangeValue(@PathVariable String from, @PathVariable String to) {
 		
+		logger.info("Retrive Exchance value is From {} to {}", from, to);
 		CurrencyExchange currencyExchange =  repository.findByFromAndTo(from, to);
 		if(currencyExchange == null) {
 			throw new RuntimeException("Unable to Find data for "+from+" to "+to);
@@ -28,6 +33,11 @@ public class CurrencyExchangeServiceController {
 		String port = environment.getProperty("local.server.port");
 		currencyExchange.setEnvironment(port);
 		return currencyExchange;
+	}
+
+	private Logger LoggerFactory(Class<CurrencyExchangeServiceController> class1) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
